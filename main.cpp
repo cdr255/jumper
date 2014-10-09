93574e27d6b3b1d0f8dad1f1b6223805890964b1
Christopher Rodriguez <cdr255@gmail.com>
2014-10-09 18:07:33 -0400

    Removed Circle and Added Sprite, Ted.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,16 @@
 
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
+  sf::RenderWindow window(sf::VideoMode(640, 480), "SFML works!");
+  sf::Texture ted_front;
+  if(!ted_front.loadFromFile("Ted.png"))
+    {
+      //error
+      return 101;
+    }
+  sf::Sprite player;
+  player.setTexture(ted_front);
+  
 
   while (window.isOpen())
     {
@@ -16,7 +23,7 @@ int main()
         }
 
       window.clear();
-      window.draw(shape);
+      window.draw(player);
       window.display();
     }
 
